6087e54e6dc07562148ab339b0b2d0c3a8b16acb
22bca222 <127926369+22bca222@users.noreply.github.com>
2023-04-03 13:34:55 +0530

    Add files via upload

---
+++ STRING SOLUTION/SCRIPT2.py
@@ -0,0 +1,6 @@
+def palindrom(word):
+   return word == word[::-1]
+sentc = [w for w in input ("enter a sentence:  ").split() if palindrom(w)]
+print("there are {} palindroms in your sentence:\n".format(len(sentc)))
+for pal in sentc:
+    print(" {} is palindrom.".format(pal))
